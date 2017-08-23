14d4455ad10ab65f2c64a31c1021fd47ce8287e3
Roman Lacko <xlacko1@fi.muni.cz>
2017-08-23 22:45:53 +0200

    Add back accidentally removed WBM::Fail

---
+++ WBM/Fail.pm
@@ -0,0 +1,47 @@
+package WBM::Fail;
+
+use v5.26;
+use utf8;
+use strict;
+use warnings;
+
+use parent      qw(WBM::Driver);
+use feature     qw(signatures);
+no  warnings    qw(experimental::signatures);
+
+sub new($class, %args) {
+    my $self = $class->SUPER::new(%args);
+
+    $self->log->fatal("missing 'text' parameter in constructor")
+        unless defined $args{text};
+
+    $self->{text}   = $args{text};
+    $self->{first}  = 1;
+    return $self;
+}
+
+sub refresh_on_event($self) { 1; }
+
+sub invoke($self) {
+    $self->log->info("invoked");
+    my $ret = {
+        text    => $self->{text},
+        icon    => "",
+        color   => "dc322f",
+        cache   => "+inf",
+    };
+
+    if ($self->{first}) {
+        $ret->{blink}       = 30;
+        $ret->{invert}      = "+inf";
+        $self->{first}      = 0;
+    } else {
+        $ret->{reset_all} = 1;
+    }
+
+    return $ret;
+}
+
+# vim: syntax=perl5-24
+
+1;
